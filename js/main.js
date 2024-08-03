0a32e372632358e509520539702f03ade77a3a20
bacelete <arthurbt2005@gmail.com>
2024-08-03 00:24:11 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded",function(){
-    
+    var diagramaAtual = document.getElementById('diagrama-atual');
+    var divImg = document.querySelector('.div-img'); 
+    var modalImg = document.querySelector('.modal-img');
+    var caption = document.querySelector('.caption'); 
+
+    diagramaAtual.onclick = function () {
+        divImg.style.display = "block"
+        modalImg.src = this.src; 
+        caption.textContent = this.alt; 
+    }
+
+    var closeBtn = document.querySelector('.close');
+    closeBtn.onclick = function () {
+        divImg.style.display = "none"; 
+    }
 })
